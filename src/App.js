93e0cc7f6ef0e206144f6557f0aef23974bb670f
maricd7 <dejanmarci259@gmail.com>
2023-11-13 12:56:41 +0100

    adding some stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Layout } from './components/layout';
 function App() {
   return (
     <div className="App">
+      <Layout/>
     </div>
   );
 }
---
+++ src/components/layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Nav } from './Nav'
+
+export const Layout = ({children}) => {
+  return (
+    <div>
+        <Nav/>
+        <main>{children}</main>
+    </div>
+  )
+}
