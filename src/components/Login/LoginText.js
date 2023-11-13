5ffdf376b1b7c5d4415f78518dbfde05100276a2
unknown <dejanmaric259@gmail.com>
2023-11-13 20:57:02 +0100

    installed react router

---
+++ src/components/Login/LoginText.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Heading } from "../common";
+export const LoginText = () => {
+  return (
+    <div>
+      <Heading text="Unlock Your Fitness Potential with LiftLink" />
+    </div>
+  );
+};
---
+++ src/pages/landing.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { LoginText } from '../components/Login'
+
+export const landing = () => {
+  return (
+    <div>
+        <LoginText/>
+    </div>
+  )
+}
