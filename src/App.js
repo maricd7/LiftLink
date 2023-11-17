1e2d434402302034db7e839b9a0760221bc9d9e8
maricd7 <dejanmarci259@gmail.com>
2023-11-17 13:47:20 +0100

    making auth work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
-import { Layout } from './components';
-import {BrowserRouter , Routes, Route } from "react-router-dom"
-import {Landing} from './pages'
-import { Home } from './pages/Home';
-
-
-
+import "./App.css";
+import { Layout } from "./components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from "./pages";
+import { Home } from "./pages/Home";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
+
+    <AuthProvider>
     <div className="App">
-      <BrowserRouter >
-        <Routes>
-          <Route path="/" element={<Landing/>  } />
-          <Route path='/home' element={<Home/>} />
-        </Routes>
-      </BrowserRouter >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
     </div>
+    </AuthProvider>
   );
 }
-
 
 export default App;
--- src/components/Login/Register.js
+++ src/components/Login/Register.js
@@ -1,54 +1,76 @@
-import React, { useRef, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { CtaButton, Input } from '../common'
-import styles from './Form.module.css'
-import { useAuth, currentUser,signup} from '../../contexts/AuthContext'
+import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { CtaButton, Input } from "../common";
+import styles from "./Form.module.css";
+import { useAuth } from "../../contexts/AuthContext";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../utils/firebase";
 
+export const Register = ({ toggleLogin, toggleRegister }) => {
+  const emailRegisterRef = useRef();
+  const passwordRegisterRef = useRef();
+  const passwordConfirmRef = useRef();
+  const [error, setError] = useState("");
 
-export const Register = ({toggleLogin, toggleRegister}) => {
-
-    const emailRef = useRef();
-    const passwordRef = useRef();
-    const passwordConfirmRef = useRef(); 
-    // const signup = useAuth();
-    const[error,setError] = useState('')
-    const[loading,setLoading]= useState(false)
-    const { signup } = useAuth();
-    const {currentUser} = useAuth();
-    async function handleSubmit(e){
-      // console.log(signup)
-        e.preventDefault(); 
-        console.log(currentUser, signup)
-        if(passwordRef.current.value !== passwordConfirmRef.current.value){
-            return(setError('Passwords dont match.'))
-        }
-        try{
-          
-          setError('');
-          setLoading(true);
-          await signup(emailRef.current.value, passwordRef.current.value);
-          
-        }catch{
-          setError('Failed to create account.')
-        }
-        setLoading(false)
+  const register = async (e) => {
+    e.preventDefault()
+    if (
+      passwordRegisterRef.current.value !== passwordConfirmRef.current.value
+    ) {
+      return setError("Passwords do not match");
     }
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        emailRegisterRef.current.value,
+        passwordRegisterRef.current.value
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  const login = () => {};
+  const logout = () => {};
   return (
     <div className={styles.container}>
       <div className={styles.formText}>
-        <h2>Register</h2> 
+        <h2>Register</h2>
         <p>Login or register into your liftlink account.</p>
       </div>
-      <form onSubmit={handleSubmit}>
-      <Input placeholder='Email address' label='Email' type='email' reference={emailRef} />
-      <Input placeholder='Password' label='Pasword' type='password' reference={passwordRef}/>
-      <Input error={error} placeholder='Confirm Password' label='Confirm Password' type='password' reference={passwordConfirmRef}/>
-      <div className={styles.buttonsContainer}>
-       
-        <p className='sign-in-parag' onClick={()=>{toggleLogin(true) ; toggleRegister(false)}}>Already have an account? Sign In</p>
-      </div>
-      <CtaButton disabled={loading} type='submit' text='Sign Up'/>
+      <form onSubmit={register}>
+        <Input
+          placeholder="Email address"
+          label="Email"
+          type="email"
+          reference={emailRegisterRef}
+        />
+        <Input
+          placeholder="Password"
+          label="Pasword"
+          type="password"
+          reference={passwordRegisterRef}
+        />
+        <Input
+          error={error}
+          placeholder="Confirm Password"
+          label="Confirm Password"
+          type="password"
+          reference={passwordConfirmRef}
+        />
+        <div className={styles.buttonsContainer}>
+          <p
+            className="sign-in-parag"
+            onClick={() => {
+              toggleLogin(true);
+              toggleRegister(false);
+            }}
+          >
+            Already have an account? Sign In
+          </p>
+        </div>
+        <CtaButton type="submit" text="Sign Up" />
       </form>
     </div>
-  )
-}
+  );
+};
