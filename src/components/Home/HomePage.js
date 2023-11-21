093c996db606f01f7b8d660b1f8899a2ab921fa9
unknown <dejanmaric259@gmail.com>
2023-11-21 17:01:24 +0100

    starting timeline section

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Navbar } from '../common'
+import { Timeline } from './Timeline'
 
 export const HomePage = () => {
   return (
     <div>
         <Navbar/>
+        <Timeline/>
     </div>
   )
 }
