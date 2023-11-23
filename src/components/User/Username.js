8744f15eb117bcfbffefcd394943af931ebffdb3
maricd7 <87302839+maricd7@users.noreply.github.com>
2023-11-23 08:19:42 +0100

    Add files via upload

--- src/components/User/Username.js
+++ src/components/User/Username.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import styles from './Username.module.css'; 
 import { useAuth } from '../../contexts/AuthContext';
-
+import { Icon } from '@iconify/react';
 export const Username = () => {
     const { user } = useAuth();
     const userName = user.providerData[0].uid ;
   return (
-    <div className={styles.userName}>{userName}</div>
+    <div className={styles.userName}>
+      <Icon icon="ri:notification-2-line" color="#fefefe" />
+      <Icon icon="ri:message-3-line" color="#fefefe" />
+      {userName}
+      <Icon icon="ri:settings-4-line" color="#fefefe" />
+      </div>
   )
 }
