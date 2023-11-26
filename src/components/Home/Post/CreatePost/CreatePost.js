b20a6987e5cfd7e4fecc7c0af3c928b4c54aa46e
unknown <dejanmaric259@gmail.com>
2023-11-26 22:12:29 +0100

    connected db to app

--- src/components/Home/Post/CreatePost/CreatePost.js
+++ src/components/Home/Post/CreatePost/CreatePost.js
@@ -2,10 +2,26 @@ import React, { useState } from "react";
 import styles from "./CreatePost.module.css";
 import { CtaButton, Input } from "../../../common";
 import { Icon } from "@iconify/react";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../../../../utils/firebase";
+import { useNavigate } from "react-router-dom";
+
 
 export const CreatePost = ({ onClick }) => {
   const [title, setTitle] = useState("");
   const [postText, setPostText] = useState("");
+  const postsCollectionRef = collection(db, "posts");
+
+  let navigate =  useNavigate()
+  const createPost = async () => {
+    await addDoc(postsCollectionRef, {
+      title,
+      postText,
+      author: { name: auth.currentUser.email, id: auth.currentUser.uid },
+    });
+    onClick()
+    navigate('/home');
+  };
   return (
     <div className={styles.createPostContainer}>
       <Icon
@@ -34,7 +50,7 @@ export const CreatePost = ({ onClick }) => {
           }}
         ></textarea>
       </div>
-      <CtaButton text="Create a Post" />
+      <CtaButton text="Create a Post" onClick={createPost}/>
     </div>
   );
 };
